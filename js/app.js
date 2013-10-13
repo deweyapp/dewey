2eb1cc189395153751f1ef4ea5790f0e243153f2
Denis Gladkikh <outcoldman@gmail.com>
2013-10-12 17:08:11 -0700

    Auto load more bookmarks on scrolling.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 angular.module('Bookmarks', []).
+  /* 
+  * Filter split search string on fields conditions and after that 
+  * uses them to build special expression for default AngularJS filter.
+  *
+  * For example:
+  *
+  * - When search string is "search string" - filter will try to find a match in any object field.
+  * - When search string is "tag:search string" - filter will try to find a match only in object `tag` property.
+  * - When search string is "search title tag:search tag" - filter will try to find a match for 'search tag' in object `tag` property
+  * an 'search title' in `title` field.
+  *
+  */
   filter('bookmarksFilter', function($filter) {
     var standardFilter = $filter('filter');
     var orderBy = $filter('orderBy');
@@ -52,34 +64,56 @@ angular.module('Bookmarks', []).
     } 
   });
 
-
+/*
+* Application controller.
+*/
 function AppCtrl($scope, $filter) {
   
-  var defaultTotalDisplayed = 20;
+  // Constant: default value of how many items we want to display on main page.
+  var defaultTotalDisplayed = 40;
 
-  $scope.searchText = '';
-  $scope.bookmarks = [];
-  $scope.orders = [{title:'Title', value: 'title'},
+  $scope.searchText = ''; // Search text
+  $scope.bookmarks = []; // All bookmarks
+  $scope.orders = [{title:'Title', value: 'title'}, // Different sorting orders
                     {title:'Date created', value: 'date'},
                     {title:'Last visited', value: 'visited'},
                     {title:'Visited count', value: 'visitedCount'}
                   ];
-  $scope.currentOrder = $scope.orders[0]; // visitedCount is by default
-  $scope.bookmarkEdit = null;
-  $scope.newTag = '';
+  $scope.currentOrder = $scope.orders[0]; // visitedCount is default sorting order
+
+  // Edit tag dialog models
+  $scope.bookmarkEdit = null; // selected bookmark (for dialog)
+  $scope.newTag = ''; 
+
+  // Maximum number of items currently displayed
   $scope.totalDisplayed = defaultTotalDisplayed;
 
+  // Repository for custom tags
   var customTags = {};
 
-  var resetTotalDisplayed = function() {
-    $scope.totalDisplayed = defaultTotalDisplayed;
-  }
+  // Auto add showing bookmarks when user scroll to page down
+  var loadMorePlaceholder = $('#loadMorePlaceholder').get(0);
+  $(window).scroll(function () {
+    if (getFilteredBookmarks().length > $scope.totalDisplayed) {
+      if (loadMorePlaceholder.getBoundingClientRect().top <= window.innerHeight) {
+        $scope.totalDisplayed += defaultTotalDisplayed;
+        $scope.$apply();
+      }
+    }
+  });
 
+  // Place focus on input when show dialog
+  $("#addTagModal").on('shown', function() {
+    $(this).find("[autofocus]:first").focus();
+  });
+
+  // Get bookmarks we show on the page (in right order)
   var getFilteredBookmarks = function() {
     var bookmarksFilter = $filter('bookmarksFilter');
     return bookmarksFilter($scope.bookmarks, $scope.searchText, $scope.currentOrder.value);
   }
 
+  // Recursive bookmarks traversal (we use folders as tags)
   var enumerateChildren = function(tree, tags) {
     if (tree) {
         angular.forEach(tree, function(c) {
@@ -127,6 +161,7 @@ function AppCtrl($scope, $filter) {
     }
   }
 
+  // Get first custom tags and after this start bookmarks traversal.
   chrome.storage.sync.get('customTags', function(data) {
 
     if (data && data.customTags) {
@@ -140,16 +175,25 @@ function AppCtrl($scope, $filter) {
     });
   });
 
-  $scope.$watch('searchText', resetTotalDisplayed);
+  // When user change search string we scroll to top of the page and set total displayed items to default
+  $scope.$watch('searchText', function() {
+    $scope.totalDisplayed = defaultTotalDisplayed;
+    setTimeout(function() {
+      window.scroll(0, 0)
+    }, 10);
+  });
  
+  // On tag click we set search text
   $scope.selectTag = function(tag) {
     $scope.searchText = 'tag:' + tag;
   };
 
+  // Change sorting order
   $scope.changeOrder = function(order) {
     $scope.currentOrder = order;
   };
 
+  // Show modal dialog for adding tags
   $scope.addTag = function(bookmark) {
     $scope.bookmarkEdit = bookmark;
     $scope.newTag = '';
@@ -160,6 +204,7 @@ function AppCtrl($scope, $filter) {
     return false;
   };
 
+  // Remove all custom tags for bookmark
   $scope.removeCustomTag = function(bookmark) {
     var tags = [];
     for (var i = bookmark.tag.length - 1; i > 0; i--) {
@@ -174,6 +219,7 @@ function AppCtrl($scope, $filter) {
     chrome.storage.sync.set({'customTags': customTags});
   };
 
+  // Handler for saving custom tag for selected bookmark
   $scope.saveNewTag = function() {
     
     if ($scope.newTag && $scope.newTag.length > 0) {
@@ -189,23 +235,11 @@ function AppCtrl($scope, $filter) {
     $('#addTagModal').modal('hide');
   };
 
+  // Navigate to first showing bookmark (use it as an enter handler)
   $scope.navigateToFirst = function() {
      var result = getFilteredBookmarks();
      if (result.length > 0) {
        window.location.href = result[0].url;
      } 
   };
-
-  $scope.showMore = function() {
-    $scope.totalDisplayed += defaultTotalDisplayed;
-  };
-
-  $scope.isShowMoreVisible = function() {
-    var result = getFilteredBookmarks();
-    return (result.length > $scope.totalDisplayed);
-  };
 }
-
-$("#addTagModal").on('shown', function() {
-    $(this).find("[autofocus]:first").focus();
-});
