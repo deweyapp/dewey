702c26c96aadfc2b9850eac28c391058d9feab0a
Denis Gladkikh <outcoldman@gmail.com>
2013-10-27 11:20:20 -0700

    Move main view to partial template

--- js/bookmarksApp.js
+++ js/bookmarksApp.js
@@ -1,6 +1,10 @@
 define(['angular'], function(angular) {
 'use strict';
 
-return angular.module('bookmarksApp', []);
+return angular.module('bookmarksApp', []).
+config(['$routeProvider', function($routeProvider) {
+   $routeProvider.when('/main', {templateUrl: 'partials/main.tpl.html', controller: 'mainController'});
+   $routeProvider.otherwise({redirectTo: '/main'});
+}]);
 
 });
