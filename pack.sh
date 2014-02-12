f072777b875f3e2942f838b462bc0bc39ffe1407
Denis Gladkikh <outcoldman@gmail.com>
2014-02-11 18:21:32 -0800

    Publish version 2.0

--- pack.sh
+++ pack.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-zip -r webstore/published/chrome-bookmarks\($(date +"%F")\).zip ./ \
-  -i css/bootstrap-tagsinput.css \
-  -i css/bootstrap-theme.min.css \
-  -i css/bootstrap.min.css \
-  -i css/font-awesome.min.css \
-  -i css/style.css \
-  -i images/*.svg \
-  -i images/*.ico \
+zip -r webstore/published/dewey\($(date +"%F")\).zip ./ \
+  -i css/*.css \
+  -i images/settings.svg \
+  -i images/search.svg \
+  -i images/edit.svg \
+  -i images/loader.gif \
+  -i images/edit-gray.svg \
+  -i images/feedback.svg \
+  -i images/review.svg \
+  -i images/donate.svg \
+  -i images/contact.svg \
+  -i images/link.svg \
+  -i images/docs.svg \
+  -i images/launch.svg \
+  -i images/folder.svg \
   -i bower_components/angular/angular.min.js \
   -i bower_components/angular-ui-router/release/angular-ui-router.min.js \
   -i bower_components/jquery/jquery.min.js \
@@ -16,13 +23,10 @@ zip -r webstore/published/chrome-bookmarks\($(date +"%F")\).zip ./ \
   -i bower_components/underscore/underscore-min.js \
   -i bower_components/ui-bootstrap/dist/ui-bootstrap-custom-tpls-0.10.0.js \
   -i bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js \
-  -i js/lib/bootstrap-tagsinput.min.js \
-  -i js/lib/bootstrap-tagsinput-angular.js \
-  -i js/filters/*.js \
-  -i js/controllers/*.js \
-  -i js/services/*.js \
+  -i bower_components/color-thief/js/color-thief.js \
+  -i js/**/*.js \
   -i js/*.js \
-  -i partials/* \
+  -i partials/*.html \
   -i app.html \
   -i icon_*.png \
   -i manifest.json \
