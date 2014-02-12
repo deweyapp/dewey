9d44e7d8d0cbf51553c7f1b91c445c431a8373b9
Denis Gladkikh <outcoldman@gmail.com>
2014-02-12 06:17:45 -0800

    Add missed images to package

--- pack.sh
+++ pack.sh
@@ -2,25 +2,7 @@
 
 zip -r webstore/published/dewey\($(date +"%F")\).zip ./ \
   -i css/*.css \
-  -i images/settings.svg \
-  -i images/search.svg \
-  -i images/edit.svg \
-  -i images/loader.gif \
-  -i images/edit-gray.svg \
-  -i images/feedback.svg \
-  -i images/review.svg \
-  -i images/donate.svg \
-  -i images/contact.svg \
-  -i images/link.svg \
-  -i images/docs.svg \
-  -i images/launch.svg \
-  -i images/folder.svg \
-  -i images/options.svg \
-  -i images/pdf.svg \
-  -i images/chrome.svg \
-  -i images/chrome-32.png \
-  -i images/dewey-small.png \
-  -i images/dewey-big.png \
+  -i images/* \
   -i bower_components/angular/angular.min.js \
   -i bower_components/angular-ui-router/release/angular-ui-router.min.js \
   -i bower_components/jquery/jquery.min.js \
