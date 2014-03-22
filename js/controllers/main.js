c8f795bbd3e5446dfb4a675a0644de0eb03db6e8
Fuhrmann <fuhrmanns@gmail.com>
2014-03-22 07:00:50 +0000

    Allow to use up/down arrow keys

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -2,20 +2,20 @@ define(
 'controllers/main',
 [
   'underscore',
-  'jQuery', 
+  'jQuery',
   'bookmarksApp',
   'services/bookmarksStorage',
   'filters/fieldsFilter',
   'controllers/editBookmark',
   'ui.bootstrap'
-], 
+],
 function(_, $, bookmarksApp) { 'use strict';
 
 /*
 * Application controller.
 */
 var MainController = function($scope, $filter, $modal, bookmarksStorage) {
-  
+
   // Constant: default value of how many items we want to display on main page.
   var defaultTotalDisplayed = 20;
 
@@ -23,16 +23,16 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
   $scope.bookmarks = []; // All bookmarks
   $scope.tags = []; // All tags
   $scope.orders = [ // Different sorting orders
-                    {title:'Date', value: 'date'}, 
-                    {title:'Title', value: 'title'}, 
-                    {title:'URL', value: 'url'} 
+                    {title:'Date', value: 'date'},
+                    {title:'Title', value: 'title'},
+                    {title:'URL', value: 'url'}
                   ];
   $scope.currentOrder = $scope.orders[0]; // date is default sorting order
 
   // Maximum number of items currently displayed
   $scope.totalDisplayed = defaultTotalDisplayed;
 
-  $scope.selectedIndex = 0; 
+  $scope.selectedIndex = 0;
 
   $scope.hideTopLevelFolders = false;
   $scope.showThumbnails = true;
@@ -48,8 +48,21 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
     }
   });
 
+  $(window).resize(function(){
+    countItemsPerRow();
+  });
+
   var getAllPanels = function() {
     return $('#list-bookmarks div.panel');
+  };
+
+  var countItemsPerRow = function() {
+    var bookmarksList = angular.element('#list-bookmarks'),
+    boxSize = bookmarksList.find('li:first-child').width(),
+    bookmarksListW = bookmarksList.width(),
+    perRow = Math.floor( bookmarksListW / boxSize);
+
+    $scope.itemsPerRow = perRow;
   };
 
   var isElementInViewport = function(el) {
@@ -71,7 +84,7 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
         var result = getFilteredBookmarks();
         if (result.length > $scope.selectedIndex) {
           window.location.href = result[$scope.selectedIndex].url;
-        } 
+        }
       }
     } else if (e.which === 37) { // left arrow key
       if ($scope.selectedIndex > 0) {
@@ -89,21 +102,31 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
         $scope.selectedIndex--;
         updated = true;
       }
-        
+
     } else if (e.which === 9) { // tab key
       if (getAllPanels().length > $scope.selectedIndex + 1) {
         $scope.selectedIndex++;
         updated = true;
       }
+    } else if (e.which === 40) { // down key
+      if (getAllPanels().length > $scope.selectedIndex + $scope.itemsPerRow) {
+        $scope.selectedIndex += $scope.itemsPerRow;
+        updated = true;
+      }
+    } else if (e.which === 38) { // up key
+      if ($scope.selectedIndex - $scope.itemsPerRow >= 0) {
+        $scope.selectedIndex -= $scope.itemsPerRow;
+        updated = true;
+      }
     }
-    if (updated) { // right arrow, left arrow, tab, and shift+tab key pressed - select next element
+    if (updated) { // right arrow, left arrow, down arrow, up arrow, tab, and shift+tab key pressed - select next element
       $scope.$apply();
       var panels = getAllPanels();
       var selectedElement = panels.get($scope.selectedIndex);
       if (selectedElement) {
         var rect = selectedElement.getBoundingClientRect(); // If element is not visible - scroll to it
         if (!(rect.top >= 0 && rect.left >= 0 && rect.bottom <= $(window).height() && rect.right <= $(window).width())) {
-          $("body").animate({
+          $("body").stop().animate({
             scrollTop: ($(panels.get($scope.selectedIndex)).offset().top - $(panels.get(0)).offset().top)
           }, 500);
         }
@@ -129,12 +152,13 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
                       .map(function (item) { return item.tag; })
                       .flatten()
                       .groupBy(function(t){ return t.text; })
-                      .map(function(tagsArray, text) {   
+                      .map(function(tagsArray, text) {
                           return {tagText: text,  numberOfTags: tagsArray.length };
                       })
                       .value();
 
       $scope.$apply();
+      countItemsPerRow();
     }.bind(this));
   }.bind(this);
   loadBookmarks();
@@ -142,7 +166,7 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
   // Set maximum total displayed items to default and scroll to top of the page
   var resetView = function() {
     $scope.totalDisplayed = defaultTotalDisplayed;
-    $scope.selectedIndex = 0; 
+    $scope.selectedIndex = 0;
     setTimeout(function() {
       window.scroll(0, 0);
     }, 10);
@@ -151,11 +175,11 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
   var updateTagsInfo = function(updatedTags, originalTags){
     var newTags = _.difference(updatedTags, originalTags);
     var deletedTags = _.difference(originalTags, updatedTags);
-    
+
     _.each(newTags, function(item){
         var existingTag = _.find($scope.tags, function(t){return t.tagText == item; });
         if(_.isUndefined(existingTag)){
-            $scope.tags.push({tagText: item, numberOfTags: 1, custom: true});  
+            $scope.tags.push({tagText: item, numberOfTags: 1, custom: true});
         }
         else{
             existingTag.numberOfTags++;
@@ -179,7 +203,7 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
   $scope.$watch('searchText', function() {
     resetView();
   });
- 
+
   // On tag click we set search text
   $scope.selectTag = function(tag) {
     _gaq.push(['_trackEvent', 'Navigation', 'selectTag']);
@@ -225,7 +249,7 @@ var MainController = function($scope, $filter, $modal, bookmarksStorage) {
         $scope.bookmarks.splice(_.indexOf($scope.bookmarks, bookmark), 1);
       }
     });
-    
+
     return false;
   };
 
