4c14930baee6c2cac047bc07f0aed06e6706cd7e
Artem Grygor <artemgrygor@gmail.com>
2014-03-27 16:35:30 -0400

    init old tests.

--- test/unit/services/booleanSearchEngine.test.js
+++ test/unit/services/booleanSearchEngine.test.js
@@ -19,6 +19,19 @@ describe('booleanSearchEngine.test.js', function() { 'use strict';
 		expect(service).to.not.be.undefined;
 		expect(service.filterBookmark).to.be.a('function');
 	});
+
+	describe('When search "string" - will try to find a match in any object field.', function(){
+
+	});
+
+	describe('When search "tag:string" - will try to find a match only in object tag property.', function(){
+
+	});
+
+	describe('When search "string1 tag:string2" - will try to find a match for search2 in object "tag" property
+an search1 title field.', function(){
+
+	});
 });
 
 });
