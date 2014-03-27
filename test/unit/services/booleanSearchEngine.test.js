c2beac219b3615f42db3360baa7f541890e97370
Artem Grygor <artemgrygor@gmail.com>
2014-03-27 16:52:56 -0400

    init dummy tests

--- test/unit/services/booleanSearchEngine.test.js
+++ test/unit/services/booleanSearchEngine.test.js
@@ -20,17 +20,22 @@ describe('booleanSearchEngine.test.js', function() { 'use strict';
 		expect(service.filterBookmark).to.be.a('function');
 	});
 
-	describe('When search "string" - will try to find a match in any object field.', function(){
-
+	describe('When search "string" - will try to find a match in any object field.', function(){	
+		it('', function(){
+			expect(service).to.not.be.undefined;
+		});
 	});
 
 	describe('When search "tag:string" - will try to find a match only in object tag property.', function(){
-
+		it('', function(){
+			expect(service).to.not.be.undefined;
+		});
 	});
 
-	describe('When search "string1 tag:string2" - will try to find a match for search2 in object "tag" property
-an search1 title field.', function(){
-
+	describe('When search "string1 tag:string2" - will try to find a match for search2 in object "tag" property an search1 title field.', function(){
+		it('', function(){
+			expect(service).to.not.be.undefined;
+		});
 	});
 });
 
