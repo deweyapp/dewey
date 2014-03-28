c4d538f9e88c4f74be9d4f7a92e777c08d49fdc1
Artem Grygor <artemgrygor@gmail.com>
2014-03-27 21:56:01 -0400

    add tests for tag pattern search and tag with whitespace

--- test/unit/services/booleanSearchEngine.test.js
+++ test/unit/services/booleanSearchEngine.test.js
@@ -66,14 +66,68 @@ describe('booleanSearchEngine.test.js', function() { 'use strict';
 	});
 
 	describe('When search "tag:string" - will try to find a match only in object tag property.', function(){
+		var bookmark, searchText;
+		
+		beforeEach(function(){
+			
+			searchText = 'tag:tag2';
+			bookmark = {
+				title: 'title',
+				url: 'http://127:0:0:1',
+				tag:[{text: 'tag1'}, {text: 'tag2'}, {text: 'tag2'}]
+			};
+		});
+
+		it('When tag contains string - result should be true', function(){
+			var isFiltered = engine.filterBookmark(bookmark, searchText);
+			expect(isFiltered).to.be.true;
+		});
+
+		it('When tag does not contain - result should be false', function(){
+			
+			searchText = 'tag:tag4';
+			var isFiltered = engine.filterBookmark(bookmark, searchText);
+			expect(isFiltered).to.be.false;
+		});
+	});
+
+	xdescribe('When search "string1 tag:string2" - will try to find a match for search2 in object "tag" property an search1 title field.', function(){
 		it('', function(){
 			expect(engine).to.not.be.undefined;
 		});
 	});
 
-	describe('When search "string1 tag:string2" - will try to find a match for search2 in object "tag" property an search1 title field.', function(){
-		it('', function(){
-			expect(engine).to.not.be.undefined;
+	describe('When search pattern contains whitespace - will try to find a match the same as without it', function(){
+		var bookmark, searchText;
+		
+		beforeEach(function(){
+			
+			searchText = 'tag: tag2';
+			bookmark = {
+				title: 'title',
+				url: 'http://127:0:0:1',
+				tag:[{text: 'tag1'}, {text: 'tag2'}, {text: 'tag2'}]
+			};
+		});
+
+		it('When tag pattern contains whitespace - result should be true', function(){
+
+			var isFiltered = engine.filterBookmark(bookmark, searchText);
+			expect(isFiltered).to.be.true;
+		});
+
+		xit('When title pattern contains whitespace - result should be true', function(){
+
+			searchText = 'title: itl';
+			var isFiltered = engine.filterBookmark(bookmark, searchText);
+			expect(isFiltered).to.be.true;
+		});
+
+		xit('When url pattern contains whitespace - result should be true', function(){
+
+			searchText = 'url: 127';
+			var isFiltered = engine.filterBookmark(bookmark, searchText);
+			expect(isFiltered).to.be.true;
 		});
 	});
 });
