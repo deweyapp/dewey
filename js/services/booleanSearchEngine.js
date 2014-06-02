3243509dcd1f0ee4839bf18a81d9304dd330f532
Artem Grygor <artemgrygor@gmail.com>
2014-06-02 17:03:51 -0400

    fix #80 for bug with "for" typeahead at Boolean operators within words

--- js/services/booleanSearchEngine.js
+++ js/services/booleanSearchEngine.js
@@ -143,7 +143,7 @@ var BooleanSearchEngine = function () {
                 return;
             }
 
-            var exps = word.split(/(\sAND\s|\sOR\s)/);
+            var exps = word.split(/(\sAND($|\s)|\sOR($|\s))/);
             _.each(exps, function(item){
                 if(isBlank(word)) return;
 
