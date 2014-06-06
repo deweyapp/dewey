3f3aec9de4630d08ff38aca43ce92325a05897f8
Jamie Wilson <jamie@jamiewilson.io>
2014-06-06 16:29:31 -0500

    added Snapito api parameter
    to reference cached images up to 30 days old

--- js/directives/updateBackground.js
+++ js/directives/updateBackground.js
@@ -1,14 +1,14 @@
 define(
 [
-  'jQuery', 
+  'jQuery',
   'color-thief'
-], 
+],
 function($, ColorThief) { 'use strict';
 
 var myUpdateBackgroundFactory = function(appSettings) {
   var thief = new ColorThief();
 
-  return function(scope, element, attrs) { 
+  return function(scope, element, attrs) {
     scope.$watch(attrs.dLoad, function(value) {
       element.on('load', function() {
         var color = null;
@@ -16,20 +16,20 @@ var myUpdateBackgroundFactory = function(appSettings) {
         try {
           color = thief.getColor(element.get(0));
         } catch(e) {}
-        
+
 
         var backgrounds = [];
 
         if (appSettings.showThumbnails) {
           backgrounds.push(
-            'url(\'http://api.snapito.com/web/sapuk-363c81e6-0e5e082b-73e5544a-71f3-4f05fc39-3/250x188?url=' + 
-              encodeURIComponent(scope.bookmark.url) + 
-              '&delay=1\')'
+            'url(\'http://api.snapito.com/web/sapuk-363c81e6-0e5e082b-73e5544a-71f3-4f05fc39-3/250x188?url=' +
+              encodeURIComponent(scope.bookmark.url) +
+              '&delay=1&freshness=-1\')'
           );
         }
 
         backgrounds.push(color ? 'rgb(' + color.join(',') + ')' : 'white');
-        
+
         var thumbnail = $('.thumbnail-loading', element.parent().parent().parent());
         thumbnail
           .removeClass('thumbnail-loading')
