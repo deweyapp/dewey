0584845f03ca28d908b1ec0ae81f265cdaecd2ac
Denis Gladkikh <denis@gladkikh.email>
2014-08-14 18:41:35 -0700

    Big screens can show more than 15 items

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -11,7 +11,7 @@ function(_, $) { 'use strict';
 var MainController = function($scope, $filter, $modal, bookmarksStorage, appSettings, booleanSearchEngine) {
 
   // Constant: default value of how many items we want to display on main page.
-  var defaultTotalDisplayed = 15;
+  var defaultTotalDisplayed = 20;
 
   $scope.searchText = ''; // Search text
   $scope.bookmarks = []; // All bookmarks
