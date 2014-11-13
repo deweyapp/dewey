45dbe6a9b99ec5f30775eb08aafb2bc9daf0770a
Artem Grygor <artemgrygor@gmail.com>
2014-11-13 10:36:18 -0500

    switch screenshot to page2images api

--- js/directives/updateBackground.js
+++ js/directives/updateBackground.js
@@ -1,32 +1,20 @@
 define(
 [
-  'jQuery',
-  'color-thief'
+    'jQuery',
+    'underscore',
+    'color-thief'
 ],
-function($, ColorThief) { 'use strict';
+function($, _, ColorThief) { 'use strict';
 
-var myUpdateBackgroundFactory = function(appSettings) {
-  var thief = new ColorThief();
+var myUpdateBackgroundFactory = function(appSettings, $http) {
+    var thief = new ColorThief();
 
-  return function(scope, element, attrs) {
-    scope.$watch(attrs.dLoad, function(value) {
-      element.on('load', function() {
+    var updateThumbnail = function(element, backgrounds){
+
         var color = null;
-
         try {
-          color = thief.getColor(element.get(0));
+            color = thief.getColor(element.get(0));
         } catch(e) {}
-
-
-        var backgrounds = [];
-
-        if (appSettings.showThumbnails) {
-          backgrounds.push(
-            'url(\'http://api.snapito.com/web/sapuk-363c81e6-0e5e082b-73e5544a-71f3-4f05fc39-3/250x188?url=' +
-              encodeURIComponent(scope.bookmark.url) +
-              '&delay=1&freshness=-1\')'
-          );
-        }
 
         backgrounds.push(color ? 'rgb(' + color.join(',') + ')' : 'white');
 
@@ -35,14 +23,45 @@ var myUpdateBackgroundFactory = function(appSettings) {
           .removeClass('thumbnail-loading')
           .addClass('thumbnail')
           .css('background', backgrounds.join(', '));
-      });
-    });
-  };
+    };
+
+    return function(scope, element, attrs) {
+        scope.$watch(attrs.dLoad, function(value) {
+            element.on('load', function() {
+       
+                var backgrounds = [];
+                if (appSettings.showThumbnails) {
+          
+                    $http({
+                        url: 'http://api.page2images.com/restfullink?p2i_url=' +
+                        encodeURIComponent(scope.bookmark.url) +
+                        '&p2i_device=6&p2i_screen=1024x768&p2i_imageformat=jpg&p2i_wait=0&p2i_key=7cd903b37d087238',
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json; charset=utf-8'
+                        }
+                    }).success(function(responseData) {
+
+                        if(!_.isUndefined(responseData.image_url)){
+                            backgrounds.push('url(' + responseData.image_url + ')');  
+                        }
+                        updateThumbnail(element, backgrounds);
+                    }).error(function(data, status, header, config) {
+                        updateThumbnail(element, backgrounds);
+                    });
+                }
+                else{
+                    updateThumbnail(element, backgrounds);
+                }
+            });
+        });
+    };
 };
 
 return [
-  'appSettings',
-  myUpdateBackgroundFactory
+    'appSettings',
+    '$http',
+    myUpdateBackgroundFactory
 ];
 
 });
